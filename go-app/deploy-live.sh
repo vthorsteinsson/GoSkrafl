eda37895da280ca1e7dce2efb8c0da8f33e34404
Vilhjalmur Thorsteinsson <vt@extrada.com>
2024-07-17 13:21:12 +0000

    Added deployment scripts

---
+++ go-app/deploy-live.sh
@@ -0,0 +1,7 @@
+
+# Deploy the GoSkrafl server to Google App Engine
+if [[ $1 == "" ]]; then
+    echo "Version is missing. Usage: deploy.sh <version>"
+    exit 1
+fi
+gcloud app deploy --project=explo-live --no-promote --version=$1 --no-cache
